96ab381d7d994df86cd63585227066c144b5b5d1
ClaudineiMS <claudineimoreirax7l@gmail.com>
2026-05-12 16:47:11 -0300

    fix: remoção do redoc por está redundante

--- config/urls.py
+++ config/urls.py
@@ -17,10 +17,5 @@ urlpatterns = [
         'api/docs/',
         SpectacularSwaggerView.as_view(url_name='schema'),
         name='swagger-ui',
-    ),
-    path(
-        'api/redoc/',
-        SpectacularRedocView.as_view(url_name='schema'),
-        name='redoc',
-    ),
+    )
 ]
