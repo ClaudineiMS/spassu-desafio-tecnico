49b49eabd825e1dabba0ab7c33e9f3bdcd3b2b82
ClaudineiMS <claudineimoreirax7l@gmail.com>
2026-05-13 09:32:56 -0300

    feat: levando as configurações do banco para a env

--- config/settings.py
+++ config/settings.py
@@ -185,12 +185,29 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+DB_ENGINE = config('DB_ENGINE', default='sqlite')
+
+if DB_ENGINE == 'postgres':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': config('POSTGRES_DB'),
+            'USER': config('POSTGRES_USER'),
+            'PASSWORD': config('POSTGRES_PASSWORD'),
+            'HOST': config('POSTGRES_HOST', default='localhost'),
+            'PORT': config('POSTGRES_PORT', default='5432'),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': config(
+                'SQLITE_NAME',
+                default=str(BASE_DIR / 'db.sqlite3'),
+            ),
+        }
+    }
 
 
 # Password validation
