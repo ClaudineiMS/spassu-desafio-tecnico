074b54439fab1d53152b50c80e90ab072aa2fc25
ClaudineiMS <claudineimoreirax7l@gmail.com>
2026-05-14 16:27:39 -0300

    feat: criação das interfaces para interagir com os dados do backend

---
+++ frontend/src/types/venda.ts
@@ -0,0 +1,46 @@
+export interface ClienteResumo {
+    id: number;
+    nome: string;
+    email?: string;
+    telefone?: string
+}
+
+export interface VendedorResumo {
+    id: number;
+    nome: string;
+    email?: string;
+    telefone?: string
+}
+
+export interface ProdutoResumo {
+    id: number;
+    codigo: string;
+    descricao: string;
+    valor_unitario: string;
+    percentual_comissao: string
+}
+
+export interface ItemVenda {
+    id?: number;
+    produto: number | ProdutoResumo;
+    quantidade: number
+}
+
+export interface Venda {
+    id: number;
+    numero_nota_fiscal: string;
+    data_hora: string;
+    cliente: number;
+    cliente_nome: string,
+    vendedor: number;
+    vendedor_nome: string,
+    itens: ItemVenda[],
+    valor_total: number
+}
+
+export interface PaginatedResponse<T> {
+    count: number;
+    next: string | null;
+    previous: string | null;
+    results: T[]
+}
